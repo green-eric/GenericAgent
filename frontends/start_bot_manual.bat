0aeeed0b47c3563dd84b6839cb4dee7048387098
Eric <green_eric2025@outlook.com>
2026-05-23 10:21:03 +0800

    [auto] rontends/wechatapp.py, emory/global_mem.txt, emory/global_mem_insight.txt, eflect/scheduler.py, emp/TODO.txt +4 (2026-05-23 10:21)

---
+++ frontends/start_bot_manual.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Starting WeChat Bot...
+cd /d D:\GenericAgent\frontends
+start "WeChat Bot" D:\GenericAgent\python\python.exe wechatapp.py
+echo Bot started in new window.
---
+++ frontends/start_wechatbot.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d D:\GenericAgent\frontends
+D:\GenericAgent\python\python.exe wechatapp.py
